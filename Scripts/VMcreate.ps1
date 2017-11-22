db405d7ea6de372dc2abae368d2f6311ae2e922f
Darith1974 <davidflynn12@hotmail.com>
2017-11-22 22:01:54 +0000

    Update VMcreate.ps1

--- Scripts/VMcreate.ps1
+++ Scripts/VMcreate.ps1
@@ -1,8 +1,8 @@
 Add-AzureAccount
 [string]$b = "hello1" + (Get-Date).Day + (Get-Date).Minute + (Get-Date).Second
 $vm1 = New-AzureVMConfig -Name $b -InstanceSize "Small" -Image "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-201409.01-en.us-127GB.vhd"
-$pwd = ""
-$un = ""
+$pwd = "some@pass1"
+$un = "mwasham"
 $vm1 | Add-AzureProvisioningConfig -Windows -AdminUserName $un -Password $pwd
 $service = $b
 $location = "West Europe"
